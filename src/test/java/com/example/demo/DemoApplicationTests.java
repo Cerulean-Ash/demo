4a3f732786fb741b37bd2d81980b292bb6f6a0f7
Ashley Lampard <ashleylampard@live.com>
2025-07-13 18:06:39 +0100

    cleanup

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	private ApplicationContext context;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(context, "The Spring application context should not be null, indicating it loaded successfully.");
 	}
 
 }
